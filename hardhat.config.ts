eca545f5ddf67f8ef130281cea2490f2c9bdb22f
Raulinho-A <raul0207abanto@gmail.com>
2025-11-24 22:40:48 -0500

    feat: add Etherscan verification configuration and remove dotenv dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,4 @@
 import type { HardhatUserConfig } from "hardhat/config";
-
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable } from "hardhat/config";
 
@@ -40,6 +39,11 @@ const config: HardhatUserConfig = {
       chainType: "l1",
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: configVariable("ETHERSCAN_API_KEY"),
     },
   },
 };
---
+++ tasks/pct.ts
@@ -0,0 +1,10 @@
+import { task } from "hardhat/config";
+
+// task("pct:balance", "Muestra el balance PCT de una address")
+//   .addParam("account", "Address a consultar")
+//   .setAction(async ({ account }, { ethers }) => {
+//     const addr = "0xc88eE28E5a33407037CA1E589E15678C5b91DfA5";
+//     const token = await ethers.getContractAt("PetCareToken", addr);
+//     const bal = await token.balanceOf(account);
+//     console.log("Balance:", ethers.formatUnits(bal, 18), "PCT");
+//   });
